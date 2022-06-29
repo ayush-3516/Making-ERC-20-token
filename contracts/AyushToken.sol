5521d4c297729422745616abb0d0479184052cc1
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-06-29 16:42:02 +0530

    Update AyushToken.sol

--- contracts/AyushToken.sol
+++ contracts/AyushToken.sol
@@ -1,14 +1,20 @@
 pragma solidity ^0.8.15;
-import @openzeppelin/contracts/token/ERC721/ERC721.sol;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-contract AyushToken is ERC20{
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract AyushToken is ERC20 {
     address public admin;
-    constructor() ERC20('Ayush', 'ACX') {
-        _mint(msg.sender, 10000 * 10 ** 18);
+
+    constructor() ERC20("Ayush", "ACX") {
+        _mint(msg.sender, 10000 * 10**18);
         admin = msg.sender;
     }
-    function mint(address to, uint amount) external {
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, "only admin");
         _mint(to, amount);
     }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
 }
-    
